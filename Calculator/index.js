88afc1149455adfd15ba2638af3ea419f5625b8e
Youssef04544 <youssefbenyedder04@gmail.com>
2022-01-09 18:20:33 +0100

    Final version of calculator

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,7 +1,7 @@
 const calculator = document.querySelector(".theCalculator");
 const output = document.querySelector(".output");
 
-const myOperators = ["+", "-", "/", "="];
+const myOperators = ["+", "-", "/", "*", "="];
 const rows = [
   ["1", "2", "3", "+"],
   ["4", "5", "6", "-"],
@@ -9,7 +9,22 @@ const rows = [
   ["C", "0", "=", "/"],
 ];
 const handleClick = (e) => {
-  console.log(e.target.innerHTML);
+  let input = e.target.innerHTML;
+  let outputValue = output.innerHTML;
+  if (outputValue === "0") outputValue = "";
+  if (myOperators.includes(input) && outputValue !== "") {
+    if (myOperators.includes(outputValue[outputValue.length - 1])) {
+      outputValue = outputValue.substring(0, outputValue.length - 1);
+    } else {
+      outputValue = eval(outputValue);
+    }
+    if (input !== "=") outputValue += input;
+  } else {
+    outputValue += input;
+  }
+
+  if (input === "C") outputValue = "0";
+  output.innerHTML = outputValue;
 };
 
 document.addEventListener("DOMContentLoaded", () => {
